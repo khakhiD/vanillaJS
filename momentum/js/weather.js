6bb4cf5c68890ec0f40789c5ae92eab923880837
khakhiD <bfwanso@naver.com>
2022-07-04 00:21:57 +0900

    Vanilla JS lecture has done

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,11 +1,16 @@
-const API_KEY = "9134246dadbc6636020c7e35cfd4f54f";
-
+const API_KEY = "db1611a1b24adb1929a97532f7872dde";
 
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    fetch(url).then(response => response.json().then(data => {
+        const city = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector("#weather span:first-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }));
 }
 function onGeoFailed() {
     console.log("Can't find you. No weather for you.");
